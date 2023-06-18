45fd64a3e349e5c90b8af2a6d0f05c966f4c11f5
bmosayebi <behnam.mosayebi80@gmail.com>
2023-06-18 09:22:46 +0330

    init

---
+++ main.py
@@ -0,0 +1,31 @@
+from scenarios import mendacity, libel, false_negative_rumor, false_positive_rumor
+
+
+
+def main():
+
+    while True:
+        print('press e to exit')
+        print('1: mendacity \n2: libel \n3: false_negative_rumor \n4: false_positive_rumor')
+        a = input('which scenario do you want?(1,2,3,4)')
+
+        if a == 'e':
+            break
+        
+        a = int(a)
+
+        if a == 1:
+            mendacity()
+
+        elif a == 2:
+            libel()
+
+        elif a == 3:
+            false_negative_rumor()
+
+        elif a == 4:
+            false_positive_rumor()
+
+
+if __name__ == '__main__':
+    main()
